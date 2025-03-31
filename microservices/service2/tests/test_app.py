d87c57c0358b0e8ed79d4c83769438c12e73ded0
Ohad Leshem <71425034+leshwebdev@users.noreply.github.com>
2025-03-31 11:40:47 +0300

    Update test_app.py

--- microservices/service2/tests/test_app.py
+++ microservices/service2/tests/test_app.py
@@ -1,11 +1,13 @@
 import pytest
 from app import app
-import sys  # Add this import
+#import sys  # Add this import
+
 
 @pytest.fixture
 def client():
     app.testing = True
     return app.test_client()
+
 
 def test_home(client):
     response = client.get('/')
